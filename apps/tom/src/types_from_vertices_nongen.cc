d7e4c13f1a76bf754f43b085de942f49724a1954
Silke Horn <silke@solros.de>
2012-01-03 13:25:45 +0000

    added GPL header in every file
    git-svn-id: https://isem.mathematik.tu-darmstadt.de/svn/smoeser@309 ccf2bdad-f0f3-450a-b28a-bacdbeb646cd

--- apps/tom/src/types_from_vertices_nongen.cc
+++ apps/tom/src/types_from_vertices_nongen.cc
@@ -1,3 +1,22 @@
+// Copyright (c) 2012 Silke Möser
+// http://solros.de/polymake/tropmat
+// 
+// This file is part of the polymake extension tropmat.
+// 
+// tropmat is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// tropmat is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with tropmat.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include "polymake/client.h"
 #include "polymake/list"
 #include <iostream>
